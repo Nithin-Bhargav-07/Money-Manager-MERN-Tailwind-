ebac83485c4e28b602465f09b09bcb70a2653ec4
Nithin Bhargav G <231001.cs@rmkec.ac.in>
2026-02-06 20:22:35 +0530

    Update API base URL to production endpoint

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api'
+  baseURL: 'https://monvexa.onrender.com/api' 
 });
 
 api.interceptors.request.use((config) => {
@@ -14,5 +14,3 @@ api.interceptors.request.use((config) => {
 });
 
 export default api;
-
-
